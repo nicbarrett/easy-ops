617fa215403b9736476ad76df8ea9a110138f1ef
nic barrett <nicbarrett2152@gmail.com>
2025-08-24 22:24:31 -0500

    clean up

--- src/test/java/com/iowaicecreamconcepts/api/auth/controller/AuthControllerTest.java
+++ src/test/java/com/iowaicecreamconcepts/api/auth/controller/AuthControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.UUID;
